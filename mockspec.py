11372495802eeb6737c45b68f8b882404f0cb6fe
Jacob Vander Vliet <jrvliet@gmail.com>
2015-08-18 19:35:43 -0600

    Finished outlining mockspec.py

--- mockspec.py
+++ mockspec.py
@@ -4,17 +4,24 @@
 #  Full pipeline for performing synthetic QSO observations
 #  of ART simulations of galaxy CGM
 
+# General libraries
 import numpy as np
 import tables as tb
-from mockspec_funcs import *
-from genLOS import *
-import subprocess as sp
-from ratesControl import *
 import os
 import pandas
+import subprocess as sp
+
+# Code specific libraries
+from files import *
+from genLOS import *
+from ratesControl import *
+
+
+
 pathname = os.path.dirname(sys.argv[0])
 codeLoc = os.path.abspath(pathname)
 summaryLoc = codeLoc+'/summaries/'
+
 
 #  Read in the control file
 print 'Reading in control file...'
@@ -32,11 +39,11 @@ print 'Incline:      ', incline
 print 'EWCut:        ', ewcut
 print 'SNR:          ', snr
 print 'NCores:       ', ncores
-print 'Root Loc:     ',rootLoc
+print 'Root Loc:     ', rootLoc
 print 'Requried Loc: ', requiredLoc
 
 # Generate gal_props.dat file
-print 'Generating gal_props.dat...'
+print 'Generating gal_profgrep ps.dat...'
 setupGalprops( galID, expn, requiredLoc, summaryLoc )
 
 
@@ -70,9 +77,73 @@ genLines(galID, gasfile, summaryLoc, expn, incline, nlos, maximpact, ncores)
 print '\nRunning LOS through box...'
 runCellfinder(codeLoc)
 
-##### 
-#  
-#  ID the cells along LOSs in the ion boxes
-#
-#####
+
+
+# Start looping over ions
+for ion in ions:
+
+
+    # Setup the ion directory
+    ionloc = setup_ion_dir(ion, galID, expn) 
+
+    # Move into the ion directory
+    os.chdir(ionloc)
+
+    ##### 
+    #  
+    #  los7
+    #  Determine path length through each cell and create the
+    #  .losdata and .lines files
+    #
+    #####
+    print '\nDetermining cell path length and applying rough cut'
+    los7(codeLoc)
+
+
+    ##### 
+    #  
+    #  specsynth
+    #  Generate the synthetic spectra
+    #
+    #####
+    print '\nGenerating spectra'
+    specsynth(codeLoc)
+
+
+    ##### 
+    #  
+    #  sysanal
+    #  Analyze the spectra
+    #
+    #####
+    print '\nAnalyzing spectra...'
+    sysanal(codeLoc)
+
+
+
+    ##### 
+    #  
+    #  cullabs
+    #  Create sysabs file
+    #
+    #####
+    print '\nCreating sysabs'
+    sysabs(codeLoc)
+
+    
+
+    ##### 
+    #  
+    #  locatecells
+    #  Identify the cells that are significant contributers to the absorption
+    #
+    #####
+    print '\nIdentifying significant cells'
+    runLocateCells(codeLoc)
+
+    os.chdir('..')
+
+
+
+
 
