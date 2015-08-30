568104ee404736a25c83e4cc443763eb68a06dcf
Jacob Vander Vliet <jrvliet@gmail.com>
2015-08-30 17:09:56 -0600

    Fixed rates makefile to enable reading large boxes

--- funcs/rates/getcube.h
+++ funcs/rates/getcube.h
@@ -1,5 +1,5 @@
       integer          maxcells,nhdr,Nlmax
       
-      parameter(maxcells = 4000000,   ! maximum number of cells in a data cube
+      parameter(maxcells = 40000000,   ! maximum number of cells in a data cube
      +          nhdr     = 2,         ! number of header lines in *GZ* data cube
      +          Nlmax    = 13)        ! maximum number of cell levels
