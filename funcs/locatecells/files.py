df6fd3165f74bb72fa1a0cd4b12a78295ffb8796
Jacob Vander Vliet <jvander@g2.hpc.swin.edu.au>
2015-09-14 17:23:00 +1000

    Voigt function added

--- funcs/locatecells/files.py
+++ funcs/locatecells/files.py
@@ -1,8 +1,10 @@
 # Functions for manipulating files for generating
 # spectra
 
+import sys
+from astropy import constants as const
 
-def config_spec(instrument):
+def config_spec(instrument, vmax, zabs, lamb0):
 
     """
     Reads Mockspec.instruments and get the properties for
@@ -10,12 +12,22 @@ def config_spec(instrument):
     
     Accepts:
         instrument (name of instrument)
+        vmax (velocity range of the spectrum to make)
+        zabs (redshift of the absorption feature)
+        lamb0 (central wavelength of the transition)
 
     Returns: 
         R_isf (resolution)
         presel (pixels per resolution element)
-        RN (read noise)
+        rn (read noise)
+        waveMin (minimum wavelegth of absorption window)
+        waveMax (maxiumum wavelength of absorption window)
+        wcen (central wavelengh of absorption window)
+        dwave (wavelength step size of sprectrum to make)
+
     """
+    found = 0
+    ckms = const.c.to('km/s').value
 
     filename = 'Mockspec.instruments'
     with open(filename) as f:
@@ -26,8 +38,91 @@ def config_spec(instrument):
         # Loop through file and locate the instrument name
         for line in f:
             
+            instr = line.split()[0]
+            if instr == instrument: 
+                found = 1
+                R_isf = float(line.split()[1])
+                presel = float(line.split()[2])
+                rn = float(line.split()[3])
+                velMin = -1*vmax
+                velMax = vmax
+
+    if found == 1:
+
+        # Compute the pixelization of the instrument
+        waveMin = (1. + velMin/ckms) * (1+zabs) * lamb0
+        waveMax = (1. + velMax/ckms) * (1+zabs) * lamb0
+        wcen = (waveMax + waveMin) / 2.0
+        dwave = wcen / (presel*R_isf)
+
+        return R_isf, presel, rn, waveMin, waveMax, wcen, dwave
+
+    else:
+        print 'Instrument {0:s} not found in {1:s}'.format(instrument, filename)
+        print 'Exiting...'
+        sys.exit()
+    
 
 
     
+def get_transitions(ion)
+    """
+    Reads in Mockspec.transitions to get the physical properties of 
+    the transition the spectrum is being made for. 
+
+    Accepts:
+        ion (name of ion, such as CIV or MgII)
+
+    Returns: 
+        k (species of ion)
+        j (ionization state of ion)
+        transName (name of the transition)
+        lamb0 (central wavelength)
+        fosc (oscillator strength)
+        gamma (damping constant)
+        mamu (atomic mass of ion in amu)
+        abund (abundance of ion)
+        ip (ionization potential from ground in eV)
+    """
     
+    found = 0
+
+    filename = 'Mockspec.transitions'
+   
+    with open(filename) as f:
+
+        # One header line
+        f.readline()
+
+        # To be a match, column 0 (on or off) and column 4 (ion name)
+        # need to match
+        for line in f:
+            
+            l = line.split()
+            
+            if l[0]=='1' and l[4]==ion:
+                found = 1
+                k = int(l[2])
+                j = int(l[3])
+                transName = l[5]
+                lamb0 = double(l[6])
+                fosc = double(l[7])
+                gamma = double(l[8])
+                mamu = double(l[9])
+                abund = double(l[10])
+                ip = double(l[11])
+                break
+
+    if found==1:
+        return k, j, transName, lamb0, gamma, abund, ip
+    else:
+        print 'Cannot find ion={0:s} in {1:s}'.format(ion, filename)
+        sys.exit()
     
+
+
+
+
+
+
+
--- funcs/locatecells/model.py
+++ funcs/locatecells/model.py
@@ -4,7 +4,7 @@ import numpy as np
 #from scipy import constants as sc
 from astropy import constants as const
 
-def setatomic(lam0, f0, gamma0):
+def set_atomic(lamb0, f0, gamma0):
 
     c = const.c.cgs.value
     e = const.e.cgs.value
@@ -12,9 +12,114 @@ def setatomic(lam0, f0, gamma0):
 
     con1Const = ( np.sqrt(np.pi)*e*e ) / ( m_e*c*c )
     con2Const = 4 * np.pi * c 
-    con1 = 1e5 * f0 * lam0*lamb0 * con1Const
+    con1 = 1e5 * f0 * lamb0*lamb0 * con1Const
     con2 = 1e-8 * gamma0 * lamb0*lamb0 / con2Const
 
 
     return con1, con2
+
+
+
+
+def init_spectrum():
+    """
+    Initilizes the spectrum to proper pixelization 
+    and unity value
+    
+    Accepts:
+        waveMin (minimum wavelength in the spectrum to make)
+        waveMax (maximum wavelength in the spectrum to make)
+        dwave (pixel sampling rate)
+
+    Returns:
+        lamb (array of wavelegth values)
+        wrkflux (array of flux values, all set to 1)
+
+    """
+
+    # Given the wavelength range and the pixel sampling rate, 
+    # determine the number of pixels "ndata" in the spectrum
+    ndata = int( (waveMax - waveMin)/dwave ) + 1
+
+    # Fill the wavelength pixel values and initialize the 
+    # spectrum as unitu continuum in array wrkflux
+
+    lamb = np.zeros(ndata)
+    wrkflux = np.ones(ndata)
+    for i in range(0,ndata):
+        lamb[i] = waveMin + (i-1)*dwave
+
+    return lamb, wrkflux
+
+
+
+
+def do_abs_lines():
+
+
+    """
+    Uses Voigt profile formalism and backgorund source
+    radiative transfer (no source function) to compute
+    the absorption lines
+    
+    Some helpful definitions:
+    b1  = column density
+    b2  = rest frame central wavelength
+    b3  = rest frame Doppler width
+    y   = natural broadening in terms of doppler units
+    x   = wavelength difference in doppler units
+    w0  = rest frame wavelegnth for transition
+    w   = rest frame wavelength fro observed wavelength
+    tau = optical depth
+
+
+    Accepts:
+        lamb0 (central wavelength of transition)
+        zabs  (redshift of absorption)
+        nline (array of cell column densities)
+        zline (array of cell redshifts)
+        bline (array of cell Doppler b values)
+        con1  (constatn 2 form set_atomic)
+        con2  (constant 1 from set_atomic)
+        lamb (array of wavelengths)
+        wrkflux (array of flux values)
+
+    Returns:
+        wkrflux (array of flux values)
+
+    """
+
+    ckms = const.c.to('km/s').value
+
+    # Initalize Voight profile parameters
+    # These must be rest from quantities even though 
+    # the line may be redshifted
+    w0 = lamb0
+    b1 = nline[i]
+    b2 = w0 * (1+zline[i]) / (1+zabs)
+    b3 = w0 * abs(bline[i])/ckms
+    y  = con2 / b3
+    tcon = con1 * (b1/b3)
+
+    # Loop over the pixels and perfomr the radiative transfer
+    for i in range(0,len(wrkflux)):
+        w = lamb[i] / (1+zabs)
+        x = (w-b2)/b3
+        voigt(x,y,u,v)
+        wrkflux[i] = wrkflux[i] * np.exp(-tcon*u)
+
+    return wrkflux
+    
+
+
+def do_lyman_limit():
+def drop_lines():
+
+
+
+
+
+
+
+
 
