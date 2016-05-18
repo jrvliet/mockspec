bdc7af6aad93ea60ba73e0dfc8264b263580118b
Jacob Vander Vliet <jrvliet@gmail.com>
2016-05-18 11:35:16 -0600

    Basic plotting functions added to pipeline

--- files.py
+++ files.py
@@ -55,6 +55,7 @@ def read_control_file():
     runSysanal = int(f.readline().split()[0])
     runCullabs = int(f.readline().split()[0])
     runLocateCells = int(f.readline().split()[0])
+    runPlotting = int(f.readline().split()[0])
     # Now at ion section
     # Loop over rest of file
     ions = []
@@ -73,7 +74,7 @@ def read_control_file():
 
     props = (galID, expn, nlos, maximpact, incline, ewcut, snr, ncores, rootLoc, sigcellsCut)
     flags = (runRates, runGenLOS, runCellfinder, runIdcells, runLos7, 
-             runSpecsynth, runSysanal, runCullabs, runLocateCells)
+             runSpecsynth, runSysanal, runCullabs, runLocateCells, runPlotting)
     return props, flags, ions, xh, instruments
 
 
