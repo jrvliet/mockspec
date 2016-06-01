26fb3be3adb1b26044d6d615f314b60014aee427
Jacob Vander Vliet <jrvliet@gmail.com>
2016-05-31 20:26:10 -0600

    Changed output of summary files to HDF5

--- funcs/compileFiles/mkHDF5.py
+++ funcs/compileFiles/mkHDF5.py
@@ -20,7 +20,7 @@ def sysabs_to_hdf5(codeLoc):
     allfile = glob.glob('*.ALL.sysabs')
 
     # Create the name of the HDF5 file
-    hdf5file = allfile + '.hdf5'
+    hdf5file = allfile + '.h5'
 
     # Get the header
     with open(allfile) as f:
@@ -44,7 +44,7 @@ def abscells_to_hdf5(codeLoc):
     filename = glob.glob('*.abs_cells.dat')
 
     # Create the name of the HDF5 file
-    hdf5file = filename.replace('dat','hdf5')
+    hdf5file = filename.replace('dat','h5')
 
     # Get the header
     with open(filename) as f:
@@ -70,7 +70,7 @@ def gasbox_to_hdf5(codeLoc):
     for filename in files:
 
         # Create the name of the HDF5 file
-        hdf5file = filename.replace('txt','hdf5')
+        hdf5file = filename.replace('txt','h5')
 
         # Get the header
         with open(filename) as f:
@@ -109,19 +109,23 @@ def genSummaries(galID, expn, incline, ions, numlos):
     
     for ion in ions:
 
-        outfile = '{0:s}_a{1:s}_i{2:d}_{3:s}_cellSummary.txt'.format(
+        outfile = '{0:s}_a{1:s}_i{2:d}_{3:s}_cellSummary.h5'.format(
                     galID,expn,incline,ion)
 
         # Write the header
-        header = ('LOS\tImpact\tPhi\tProbbed\tProbbed_in_halos\t'
-                    'In_Lines\tLines_in_halos\tSignificant\t'   
-                    'Sig_in_halos\n')
-        f = open(outfile, 'w')
-        f.write(header)
+        #header = ('LOS\tImpact\tPhi\tProbbed\tProbbed_in_halos\t'
+        #            'In_Lines\tLines_in_halos\tSignificant\t'   
+        #            'Sig_in_halos\n')
+        header = ['LOS','Impact','Phi','Probbed','Probbed_in_halos',
+                    'In_Lines','Lines_in_halos','Significant',   
+                    'Sig_in_halos']
+        #f = open(outfile, 'w')
+        #f.write(header)
                 
         s = ('{0:d}\t{1:.3f}\t{2:.3f}\t{3:d}\t{4:d}\t{5:d}\t{6:d}\t'
                 '{7:d}\t{8:d}\n')
-    
+        numcols = 9
+        data = np.zeros((numlos,numcols))
         # Loop over the lines of sight
         for i in range(numlos):
             
@@ -139,13 +143,23 @@ def genSummaries(galID, expn, incline, ions, numlos):
                 expn, ion, incline, losnum, probbedCells, linesCells, 
                 sigCells)
 
-
+            data[i,0] = i+1
+            data[i,1] = imp[i]
+            data[i,2] = phi[i]
+            data[i,3] = len(probbedIDs)
+            data[i,4] = probbedinSub
+            data[i,5] = len(linesIDs)
+            data[i,6] = linesinSub
+            data[i,7] = len(sigIDs)
+            data[i,8] = siginSub
             # Format the output string
-            f.write(s.format(i+1, imp[i], phi[i], len(probbedIDs), 
-                probbedinSub, len(linesIDs), linesinSub, len(sigIDs),
-                siginSub))
+            #f.write(s.format(i+1, imp[i], phi[i], len(probbedIDs), 
+                #probbedinSub, len(linesIDs), linesinSub, len(sigIDs),
+                #siginSub))
 
-        f.close()
+        #f.close()
+        df = pd.DataFrame(data, columns=header)
+        df.to_hdr(outfile, 'data', mode='w')
 
 
 
